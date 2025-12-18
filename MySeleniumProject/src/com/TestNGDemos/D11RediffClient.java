82c5879ea36ad2ceb9117c3f1cfb6a0b0af89c87
Ankush <ankushvankore@gmail.com>
2025-12-18 22:36:42 +0530

    Date: 18 Dec 2025

--- MySeleniumProject/src/com/TestNGDemos/D11RediffClient.java
+++ MySeleniumProject/src/com/TestNGDemos/D11RediffClient.java
@@ -8,15 +8,34 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 
 public class D11RediffClient {
 	WebDriver driver;
 	RediffUtility r1;
 	
 	@Test
-	public void createUser() {
+	public void createUser1() {
 		r1.setFullName("Shashikant Mall");
 		r1.setRediffId("shashikantmall");
+		r1.setPassword("Shashi!234");
+		r1.setBirthDate("10", "OCT", "1991");
+		r1.selectGender("Male");
+	}
+	
+	@Test
+	public void createUser2() {
+		r1.setFullName("Neha Sharma");
+		r1.setRediffId("nehasharma123");
+		r1.setPassword("neha!234");
+		r1.setBirthDate("15", "JUL", "1994");
+		r1.selectGender("Female");
+	}
+	
+	@BeforeMethod
+	public void clearForm() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.navigate().refresh();
 	}
 
 	@BeforeTest
@@ -27,7 +46,7 @@ public class D11RediffClient {
 
 		driver.get("https://register.rediff.com/register/register.php?FormName=user_details");
 		
-		r1 = new RediffUtility();
+		r1 = new RediffUtility(driver);
 	}
 
 	@AfterTest
--- MySeleniumProject/src/com/TestNGDemos/RediffUtility.java
+++ MySeleniumProject/src/com/TestNGDemos/RediffUtility.java
@@ -2,14 +2,47 @@ package com.TestNGDemos;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class RediffUtility {
 	WebDriver driver;
 	
+	//Page Factory ==> Object Repository
+	@FindBy (xpath = "//input[@placeholder='Enter your full name']")WebElement fullName;
+	@FindBy (id = "newpasswd")WebElement passwordTx;
+	
+	public RediffUtility(WebDriver d) {
+		driver = d;
+		PageFactory.initElements(driver, this);
+	}
+	
 	public void setFullName(String fn) {
-		driver.findElement(By.xpath("//input[@placeholder='Enter your full name']")).sendKeys(fn);
+		//driver.findElement(By.xpath("//input[@placeholder='Enter your full name']")).sendKeys(fn);
+		fullName.sendKeys(fn);
 	}
 	public void setRediffId(String rid) {
 		driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[3]/div[1]/input[1]")).sendKeys(rid);
+	}
+	
+	public void setPassword(String ps) {
+		//driver.findElement(By.xpath("//input[@id='newpasswd']")).sendKeys(ps);
+		passwordTx.sendKeys(ps);
+		driver.findElement(By.xpath("//input[@id='newpasswd1']")).sendKeys(ps);
+	}
+	
+	public void setBirthDate(String day, String month, String year) {
+		new Select(driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[8]/select[1]"))).selectByVisibleText(day);
+		new Select(driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[8]/select[2]"))).selectByVisibleText(month);
+		new Select(driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[8]/select[3]"))).selectByVisibleText(year);
+	}
+	
+	public void selectGender(String gn) {
+		if(gn.equals("Male"))
+			driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[9]/div[1]/label[1]/input[1]")).click();
+		else
+			driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[9]/div[1]/label[2]/input[1]")).click();
 	}
 }
